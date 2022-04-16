a28e6d688ce76a0190a0e38eb4782bfa156d8881
Jake <505e06b2@gmail.com>
2022-04-16 11:41:35 +0100

    UI in working state Changed API as was unecessarily complex Refactoring needed before progressing

--- api.mjs
+++ api.mjs
@@ -5,6 +5,17 @@ import * as settings from "./settings.mjs";
 
 const music = {};
 
+//only works with musicbrainz default name scheme "00 song title.opus"
+function createSongObject(artist_name, album_name, song_filename) {
+	return {
+		//artist: artist_name,
+		//album: album_name,
+		title: song_filename.slice(3, -5),
+		track: parseInt(song_filename.slice(0,3)),
+		uri: `${settings.music_uri}${artist_name}/${album_name}/${song_filename}`
+	};
+}
+
 function listDirectories(...folder_path) {
 	return fs.readdirSync(path.join(...folder_path), {withFileTypes: true})
 		.filter(x => x.isDirectory())
@@ -24,48 +35,18 @@ function caseInsensitiveSort(a, b) {
 //generate cache
 for(const artist of listDirectories(settings.music_folder)) {
 	const current_artist = {};
-	for(const album of listDirectories(settings.music_folder, artist)) {
-		const current_album = listFiles(settings.music_folder, artist, album).filter(x => x !== "cover.jpg");
+	for(const album of listDirectories(settings.music_folder, artist).sort(caseInsensitiveSort)) {
+		const current_album = listFiles(settings.music_folder, artist, album)
+			.filter(x => x !== "cover.jpg")
+			.map(x => createSongObject(artist, album, x))
+			.sort((a, b) => a.track > b.track ? 1 : -1);
 		current_artist[album] = current_album;
 	}
 	music[artist] = current_artist;
 }
 
-console.log("Loaded Media:", music);
+console.log("Loaded Media:", JSON.stringify(music, null, 2));
 
-export async function getArtists(url) {
-	return Object.keys(music).sort(caseInsensitiveSort);
-}
-
-export async function getAlbums(url) {
-	const artist = music[url.searchParams.get("artist") || ""];
-	if(!artist) return;
-
-	return Object.keys(artist).sort(caseInsensitiveSort);
-}
-
-export async function getSongs(url) {
-	//only works with musicbrainz default name scheme "00 song title.opus"
-	function createSongObject(artist_name, album_name, song_filename) {
-		return {
-			artist: artist_name,
-			album: album_name,
-			title: song_filename.slice(3, -5),
-			track: parseInt(song_filename.slice(0,3)),
-			uri: `${settings.music_uri}${artist_name}/${album_name}/${song_filename}`
-		};
-	}
-
-	const artist_name = url.searchParams.get("artist") || "";
-	const artist = music[artist_name];
-	if(!artist) return;
-
-	const album_name = url.searchParams.get("album") || "";
-	if(album_name) {
-		const album = artist[album_name];
-		if(!album) return;
-		return album.map(x => createSongObject(artist_name, album_name, x));
-	}
-
-	return;
+export async function getLibrary(url) {
+	return music;
 }
--- webpage_assets/js/main.mjs
+++ webpage_assets/js/main.mjs
@@ -1,4 +1,86 @@
 import Elements from "./elements.mjs";
 
-Elements.find('#loading').style.display = "none";
-Elements.find('#page-container').style.display = "block";
+let library;
+let content_container;
+let previous_content;
+let top_dock_path;
+
+function createFolderList(folder, onclick = () => undefined) {
+	const container = Elements.create("div", {className: "list"});
+	for(const name of Object.keys(folder)) {
+		container.append(Elements.create("a", {
+			innerText: `(${Object.values(folder[name]).length}) ${name}`,
+			href: `#${name}`,
+			title: name,
+			onclick: () => {onclick(name); return false}
+		}));
+	}
+	return container;
+}
+
+function createSongList(folder, onclick = () => undefined) {
+	const container = Elements.create("div", {className: "list"});
+	for(const x of Object.values(folder)) {
+		container.append(Elements.create("a", {
+			innerText: `${x.track.toString().padStart(3)} ${x.title}`,
+			href: `#${x.title}`,
+			title: x.title,
+			onclick: () => {onclick(x.title); return false}
+		}));
+	}
+	return container;
+}
+
+function openFolder(artist = "", album = "") {
+	let folder = library;
+	let onclick = (name) => openFolder(name);
+	Array.from(top_dock_path.children).slice(1).map(x => x.outerHTML = "");
+
+	if(artist) {
+		folder = folder[artist];
+		onclick = (name) => openFolder(artist, name);
+		top_dock_path.append(Elements.create("a", {
+			href: `#${artist}`,
+			onclick: () => openFolder(artist),
+			innerText: `${artist}/`,
+			title: artist
+		}));
+	}
+	if(!folder) throw `"${artist}" is an invalid artist`;
+
+	if(album) {
+		folder = folder[album];
+		onclick = (name) => openFile(artist, album, name);
+		top_dock_path.append(Elements.create("a", {
+			href: `#${album}`,
+			onclick: () => openFolder(artist, album),
+			innerText: `${album}/`,
+			title: album
+		}));
+	}
+	if(!folder) throw `"${album}" is an invalid album`;
+
+	if(previous_content) previous_content.delete();
+	previous_content = artist && album && createSongList(folder, onclick) || createFolderList(folder, onclick);
+	content_container.append(previous_content);
+	return false;
+}
+
+function openFile(artist, album, song) {
+	console.log("Playing", artist, album, song);
+}
+
+try {
+	library = await(await fetch("api/getLibrary")).json();
+	content_container = Elements.find('#content');
+	top_dock_path = Elements.find('#top-dock .path');
+	Elements.find('#root').onclick = () => openFolder();
+
+	openFolder();
+
+	Elements.find('#loading').style.display = "none";
+	Elements.find('#page-container').style.display = "block";
+} catch(e) {
+	Elements.find('#loading').innerText = `Error! :(\n\nSend ダフティ#0068 a message on Discord\n\n${e}`;
+	console.trace(e);
+}
