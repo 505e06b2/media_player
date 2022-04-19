9995cd1d202a8072476e361d538ff7f42794a7f4
Jake <505e06b2@gmail.com>
2022-04-19 18:16:24 +0100

    Fix webpage folder setting

--- server/settings.mjs
+++ server/settings.mjs
@@ -6,5 +6,5 @@ export const port = 5500;
 export const api_uri = "/api/";
 export const music_uri = "/music/";
 
-export const webpage_assets_folder = path.resolve("./webpage_assets");
+export const webpage_assets_folder = path.resolve("./webpage");
 export const music_folder = path.join(webpage_assets_folder, music_uri);
