57efa85fe83e6ca8c72494e7c8cb6b9b2a2aa781
Jake <505e06b2@gmail.com>
2022-04-21 23:38:32 +0100

    Put audio elem back into DOM

--- webpage/js/audio_manager.mjs
+++ webpage/js/audio_manager.mjs
@@ -16,6 +16,7 @@ export const State = {
 
 function AudioManager() {
 	const _audio = new Audio();
+	document.head.append(_audio); //give access to userscripts
 	let _current_playlist = null; //Library.Playlist
 	let _playlist_index = -1;
 	let _repeat = Repeat.playlist;
