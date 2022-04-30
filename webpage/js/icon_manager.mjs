f0c2636bc350c680ceb22d39f9958a0bce3768f2
Jake <505e06b2@gmail.com>
2022-04-30 18:02:06 +0100

    Encode URI for icon

--- webpage/js/icon_manager.mjs
+++ webpage/js/icon_manager.mjs
@@ -10,10 +10,10 @@ function IconManager() {
 	this.getIcon = () => _icon_path;
 
 	this.setColours = (fg_colour, bg_colour) => {
-		if(fg_colour == false) fg_colour = "#1fff50"; //can't use defaults as could be empty string
+		if(fg_colour == false) fg_colour = "#1fff50"; //can't use parameter defaults as could be empty string
 		if(bg_colour == false) bg_colour = "#023300";
 
-		_icon_path = `${default_icon_path}?fg=${fg_colour}&bg=${bg_colour}`;
+		_icon_path = `${default_icon_path}?fg=${encodeURIComponent(fg_colour)}&bg=${encodeURIComponent(bg_colour)}`;
 
 		Elements.find('link[rel="icon"]').href = _icon_path;
 	};
